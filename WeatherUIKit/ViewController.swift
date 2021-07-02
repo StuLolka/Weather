a9297f82fcd2e9feb1fe09da81997750a5ccd60e
Stu <gera-stu@ya.ru>
2021-07-02 13:08:18 +0300

    TTTTTTTest

---
+++ WeatherUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherUIKit
+//
+//  Created by Сэнди Белка on 02.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Test git
+    }
+
+
+}
+
